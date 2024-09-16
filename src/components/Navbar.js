e97a13ed9ae49326b3558c412789b7830d5368bf
FirstLearnJava <mat.floimayr@derflorianer.at>
2024-09-16 19:52:44 +0200

    Add navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,8 @@ import Link from 'next/link';
 import React from 'react';
 import Logo from './Logo';
 import { useRouter } from 'next/router';
+import { TwitterIcon, GithubIcon, LinkedInIcon } from './icons';
+import { motion } from 'framer-motion';
 
 const CustomLink = ({ href, title, className = '' }) => {
   const router = useRouter();
@@ -31,25 +33,34 @@ const Navbar = () => {
       <div>
         <Logo className="absolute left-[50%] top-2 translate-x-[-50%]" />
       </div>
-      <nav>
-        <Link href="/" target={'_blank'}>
-          T
-        </Link>
-        <Link href="/" target={'_blank'}>
-          T
-        </Link>
-        <Link href="/" target={'_blank'}>
-          T
-        </Link>
-        <Link href="/" target={'_blank'}>
-          T
-        </Link>
-        <Link href="/" target={'_blank'}>
-          T
-        </Link>
-        <Link href="/" target={'_blank'}>
-          T
-        </Link>
+      <nav className="flex justify-center items-center flex-wrap">
+        <motion.a
+          href="https://twitter.com/dierosamariposa"
+          target={'_blank'}
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+          className="w-6 mx-3"
+        >
+          <TwitterIcon />
+        </motion.a>
+        <motion.a
+          href="https://github.com/FirstLearnJava"
+          target={'_blank'}
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+          className="w-6 mx-3"
+        >
+          <GithubIcon />
+        </motion.a>
+        <motion.a
+          href="https://www.linkedin.com/in/matthias-floimayr-387122231/"
+          target={'_blank'}
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+          className="w-6 ml-3"
+        >
+          <LinkedInIcon />
+        </motion.a>
       </nav>
     </header>
   );
